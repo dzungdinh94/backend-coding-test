ac7a7386a37eee093f7788a828eedd8994d4983b
dzungdinh94 <dungnd8594@gmail.com>
2022-03-13 14:45:01 +0700

    impl refactoring

---
+++ src/utils/databaseQuery.js
@@ -0,0 +1,62 @@
+const selectQuery = (db, query = '', values = []) => {
+  const results = [];
+
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db.each(
+        query,
+        values,
+        (err, row) => {
+          if (err) {
+            return reject(err);
+          }
+          results.push(row);
+        },
+        (accumulatedValuesErr, count) => {
+          if (accumulatedValuesErr) {
+            return reject(accumulatedValuesErr);
+          }
+
+          return resolve({ results, count });
+        },
+      );
+    });
+  });
+};
+
+const insertQuery = (db, query = '', values = []) => new Promise((resolve, reject) => {
+  // eslint-disable-next-line prefer-arrow-callback
+  db.run(query, values, (insertErr) => {
+    if (insertErr) {
+      return reject(insertErr);
+    }
+    resolve(this.lastID);
+  });
+});
+
+const deleteQuery = (db, query = '', values = []) => new Promise((resolve, reject) => {
+  // eslint-disable-next-line prefer-arrow-callback
+  db.run(query, values, (deleteErr) => {
+    if (deleteErr) {
+      return reject(deleteErr);
+    }
+    resolve();
+  });
+});
+
+const updateQuery = (db, query = '', values = []) => new Promise((resolve, reject) => {
+  // eslint-disable-next-line prefer-arrow-callback
+  db.run(query, values, (updateErr) => {
+    if (updateErr) {
+      return reject(updateErr);
+    }
+    resolve(this.changes);
+  });
+});
+
+module.exports = {
+  selectQuery,
+  insertQuery,
+  deleteQuery,
+  updateQuery,
+};
